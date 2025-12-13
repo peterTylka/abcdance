f5506f2c3cefdd36ef5322895aa6bedfe1bf6346
Peter Tylka <ptylka@gmail.com>
2025-12-13 11:10:58 +0100

    basic read

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,41 @@ import { Dance, Dances } from '@/lib/types';
 import { DEFAULT_DANCES } from '@/lib/constants';
 import { useState } from 'react';
 
+function readAloud(texts: string[], delay: number) {
+  let index = 0;
+
+  function readNext() {
+    if (index >= texts.length) return;
+
+    console.log('%c read next', 'background-color: skyblue', {
+      texts,
+      delay,
+      index,
+      text: texts[index],
+    });
+    const utterance = new SpeechSynthesisUtterance(texts[index]);
+    utterance.onend = () => {
+      setTimeout(() => {
+        index++;
+        readNext();
+      }, delay);
+    };
+    speechSynthesis.speak(utterance);
+  }
+
+  readNext();
+}
+
 export default function Home() {
   const [dance, setDance] = useState(Dance.Bachata);
+  const [delaySeconds, setDelaySeconds] = useState(6);
 
   console.log('%c dance', 'background-color: skyblue', { dance });
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-left py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <nav className="flex gap-3 mb-3">
+        <nav className="flex flex-col gap-3 mb-3">
           {Dances.map((danceItem) => (
             <button
               key={danceItem}
@@ -25,6 +51,15 @@ export default function Home() {
               {danceItem}
             </button>
           ))}
+
+          <button
+            className="font-bold"
+            onClick={() => {
+              readAloud(DEFAULT_DANCES[dance], delaySeconds * 1000);
+            }}
+          >
+            READ FIGURES
+          </button>
         </nav>
         <div className="font-bold">Figures:</div>
         <ul>
